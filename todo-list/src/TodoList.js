fba502e694a43fb3df4b49c248bfd84b138a76b8
homxu <wanghongxu1994@qq.com>
2020-08-31 21:53:56 +0800

    add 'add' and 'delete' function

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -5,7 +5,7 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputValue: 'hello ',
+            inputValue: '',
             list: []
         }
     }
@@ -20,20 +20,47 @@ class TodoList extends Component {
                         value = { this.state.inputValue } 
                         onChange = { this.handleInputChage.bind(this) }
                     />
-                    <button>提交</button>
+                    <button onClick= {this.handleBtnClick.bind(this)}>提交</button>
                 </div>
                 <ul>
-                    <li>Learn</li>
-                    <li>React</li>
+                    {
+                        this.state.list.map((item,index) => {
+                            return (
+                                <li 
+                                    key={index}  
+                                    onClick={this.handleItemDelete.bind(this,index)}
+                                >
+                                    {item}                             
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </Fragment>
         )
     }
 
-    handleInputChage = (e) => {
+    handleInputChage = (e) => { //输入框变化事件
         this.setState({
             inputValue: e.target.value
         })
+    }
+
+    handleBtnClick() { //添加功能
+        this.setState({
+            inputValue: '',
+            list: [...this.state.list, this.state.inputValue]
+        })
+    }
+
+    handleItemDelete(index) { //删除功能
+        const list = [...this.state.list]
+        list.splice(index,1)
+
+        this.setState({
+            list: list
+        })
+        
     }
 }
 
