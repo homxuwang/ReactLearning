bfb17784c2d348b61aab094894f97a177e0e2006
homxu <wanghongxu1994@qq.com>
2020-09-03 21:11:57 +0800

    import redux && create store

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -1,30 +1,30 @@
 import React,{Component } from 'react'
 import 'antd/dist/antd.css';
 import { Input,Button,List, Typography, Divider } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.'
-];
+import store from './store/index'
 
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+    console.log(this.state)
+  }
+
   render() {
     return(
       <div>
         <div style={{marginTop:'10px',marginLeft:'10px'}}>
-          <Input placeholder='todo Info' style={{width: '300px',marginRight:'10px'}}/>
+          <Input value={this.state.inputValue} placeholder='todo Info' style={{width: '300px',marginRight:'10px'}}/>
           <Button type="primary">提交</Button>
         </div>
         <List
           style={{margin:'20px 10px 10px 10px',width:'360px'}}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (
           <List.Item>
-                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              {item}
           </List.Item>
           )}
         />
